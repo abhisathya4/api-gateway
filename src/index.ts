c4864d3f2c15f85076986b120317835ff1447c79
abhisathya4 <73118914+abhisathya4@users.noreply.github.com>
2025-07-01 08:24:30 +0530

    Modify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { app } from "./routes/app";
 
+const hostname = "localhost";
+const port = 3000;
+
 Bun.serve({
-  port: 3001,
-  hostname: "localhost",
+  port: port,
+  hostname: hostname,
   fetch: app.fetch,
 });
 
-console.log("Server started on http://localhost:3001");
-
+console.log(`Server started on http://${hostname}:${port}`);
